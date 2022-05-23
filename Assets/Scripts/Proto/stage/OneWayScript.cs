143e50723972d51de015d848b4a3080363d5b1de
Ryouta32 <ryouta32ryouta@gmail.com>
2022-05-24 04:56:21 +0900

    仮ステージ、一方通行
    たぶんMAYA使ったほうが速いです

---
+++ Assets/Scripts/Proto/stage/OneWayScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] BoxCollider bloc;
+
+    public void OnWayTriggerEnter()
+    {
+        bloc.isTrigger = true;
+    }
+    public void OnWayTriggerExit()
+    {
+        bloc.isTrigger = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Proto/stage/WayScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.parent.GetComponent<OneWayScript>().OnWayTriggerEnter();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent.GetComponent<OneWayScript>().OnWayTriggerExit();
+    }
+    void Update()
+    {
+        
+    }
+}
