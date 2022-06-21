43b1df92c35a8750268a11ec139700d2375cc321
GTM106 <raito125dragon7@gmail.com>
2022-06-22 01:04:13 +0900

    画面外で静止している団子を非アクティブにしました
    ひとまず時間は10秒としています。変更したい際はconst値のDELETE_FRAMEを変更してください。

--- Assets/Scripts/Proto/DangoManager.cs
+++ Assets/Scripts/Proto/DangoManager.cs
@@ -7,8 +7,35 @@ using UnityEngine;
 /// </summary>
 public class DangoManager : MonoBehaviour
 {
-    DangoColor dango=DangoColor.None;
+    //è¡ãééûä‘Åi10ïbÅj
+    const int DELETE_FRAME = 500;
+    const float DELETE_MIN_SPEED = 0.1f;
 
-    public DangoColor GetDangoColor() => dango;
-    public void SetDangoType(DangoColor type) => dango = type;
+
+    //è¡ãééûä‘
+    int _frameCount = DELETE_FRAME;
+
+    DangoColor _dango = DangoColor.None;
+    [SerializeField] Renderer rend = default!;
+    [SerializeField] Rigidbody rb = default!;
+
+    public void FixedUpdate()
+    {
+        if (!rend.isVisible && rb.velocity.magnitude < DELETE_MIN_SPEED)
+        {
+            _frameCount--;
+        }
+        else
+        {
+            _frameCount = DELETE_FRAME;
+        }
+
+        if (_frameCount <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public DangoColor GetDangoColor() => _dango;
+    public void SetDangoType(DangoColor type) => _dango = type;
 }
