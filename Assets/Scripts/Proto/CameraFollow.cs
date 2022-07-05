856a85a199846be8c7c3f916afe50e289e0bda71
Ryouta32 <ryouta32ryouta@gmail.com>
2022-07-05 17:58:44 +0900

    移動のみ滑らかに動くカメラ移動

--- Assets/Scripts/Proto/CameraFollow.cs
+++ Assets/Scripts/Proto/CameraFollow.cs
@@ -6,17 +6,19 @@ public class CameraFollow : MonoBehaviour
 {
     enum State
     {
-        normal,
-        Lerp,
+        normal,//プレイヤーの後をラグなくついていきます
+        Lerp,//プレイヤーの回転、移動に滑らかについていきます
+        LerpMove,//プレイヤーの移動のみに滑らかについていきます
     }
     [SerializeField, Tooltip("追従したいターゲット")] Transform target = default!;
     [SerializeField] float MinAngle;
     [SerializeField] float MaxAngle;
-    [SerializeField] float t = 1;
+    [SerializeField,Tooltip("どのくらいの時間で追いつくか")] float ratio;
 
     [SerializeField, Range(0.01f, 1f), Tooltip("カメラの追従度")]
     private float smoothSpeed = 0.125f;
 
+    [SerializeField] GameObject TerminusObjct;
     //[SerializeField, Tooltip("ターゲットからのカメラの位置")]
     //private Vector3 offset = new Vector3(0f, 1f, -10f);
 
@@ -35,7 +37,7 @@ public class CameraFollow : MonoBehaviour
     {
         _playerData = target.GetComponent<PlayerData>();
         transform.parent = null;//動くものに乗るとそれに追従しだすから親子関係を無くす
-        terminus = GameObject.Find("CameraTerminusObj");
+        terminus = Instantiate(TerminusObjct);
         terminus.transform.position = transform.position;
     }
 
@@ -44,7 +46,6 @@ public class CameraFollow : MonoBehaviour
     {
         //カメラの位置を変更する
         terminus.transform.position += target.position - targetPos;
-        targetPos = target.position;
 
         //X軸回転の角度を所得
         float currentYAngle = transform.eulerAngles.x;
@@ -54,21 +55,28 @@ public class CameraFollow : MonoBehaviour
         {
             currentYAngle -= 360;
         }
-        Rote(terminus, currentYAngle);
         switch (state)
         {
 
             case State.normal:
+                Rote(terminus, currentYAngle);
                 transform.position = terminus.transform.position;
                 transform.rotation = terminus.transform.rotation;
                 break;
             case State.Lerp:
+                Rote(terminus, currentYAngle);
                 transform.rotation = terminus.transform.rotation;
-                transform.position = Vector3.Lerp(transform.position, terminus.transform.position, t*Time.deltaTime);
+                transform.position = Vector3.Lerp(transform.position, terminus.transform.position, ratio*Time.deltaTime);
+                break;
+            case State.LerpMove:
+                transform.position = Vector3.Lerp(transform.position, terminus.transform.position, ratio * Time.deltaTime);
+                Rote(gameObject,currentYAngle);
+                Rote(terminus, currentYAngle);
                 break;
         }
         //カメラの位置が決定してからプレイヤーの向きを決めることで、カクつきをなくす。
         target.transform.rotation = Quaternion.Euler(0, transform.localEulerAngles.y, 0);
+        targetPos = target.position;
 
     }
     private void Rote(GameObject obj, float a)
