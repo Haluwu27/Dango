6376de3719d9fd39447c1fbb0c511c7ef156c537
Ryouta32 <90999621+Ryouta32@users.noreply.github.com>
2022-07-16 05:30:52 +0900

    カメラの位置を調整
    位置調整の際、滑らかに動きません。
    動かすだけなら可能なので上げときます。

--- Assets/Scripts/Proto/CameraFollow.cs
+++ Assets/Scripts/Proto/CameraFollow.cs
@@ -13,12 +13,15 @@ public class CameraFollow : MonoBehaviour
     [SerializeField, Tooltip("追従したいターゲット")] Transform target = default!;
     [SerializeField] float MinAngle;
     [SerializeField] float MaxAngle;
-    [SerializeField,Tooltip("どのくらいの時間で追いつくか")] float ratio=1;
+    [SerializeField, Tooltip("どのくらいの時間で追いつくか")] float ratio = 1;
 
     [SerializeField, Range(0.01f, 1f), Tooltip("カメラの追従度")]
     private float smoothSpeed = 0.125f;
 
     [SerializeField] GameObject TerminusObjct;
+
+    [SerializeField] LayerMask wallLayer;//マップのレイヤーマスク
+
     //[SerializeField, Tooltip("ターゲットからのカメラの位置")]
     //private Vector3 offset = new Vector3(0f, 1f, -10f);
 
@@ -32,6 +35,9 @@ public class CameraFollow : MonoBehaviour
 
     private float roteYSpeed = -10f;
 
+    Vector3 wallHitPos;//壁にぶつかった際の座標
+    RaycastHit hit;//壁を所得するRay
+
     [SerializeField] State state;
     private void Start()
     {
@@ -44,6 +50,9 @@ public class CameraFollow : MonoBehaviour
     //Playerが動いたあとに実行するため、LateUpdateで行う。
     private void LateUpdate()
     {
+
+        _playerData = target.GetComponent<PlayerData>();
+
         //カメラの位置を変更する
         terminus.transform.position += target.position - targetPos;
 
@@ -55,25 +64,59 @@ public class CameraFollow : MonoBehaviour
         {
             currentYAngle -= 360;
         }
+
+        if (WallHitCheck())
+        {
+            //switch (state)
+            //{
+
+            //    case State.normal:
+                    transform.position = wallHitPos;//Lerpｗｐ使うと移動の際に壁の中が見えるため固定
+            //        break;
+            //    case State.Lerp:
+            //        transform.position = Vector3.Lerp(transform.position, wallHitPos, ratio*Time.deltaTime);
+            //        break;
+            //    case State.LerpMove:
+            //        transform.position = Vector3.Lerp(transform.position, wallHitPos, ratio * Time.deltaTime);
+            //        break;
+            //}
+        }
+        else//カメラの移動
+        {
+            switch(state)
+            {
+                case State.normal:
+                    transform.position = terminus.transform.position;
+                    break;
+                case State.Lerp:
+                    transform.position = Vector3.Lerp(transform.position, terminus.transform.position, ratio * Time.deltaTime);
+                    break;
+                case State.LerpMove:
+                    transform.position = Vector3.Lerp(transform.position, terminus.transform.position, ratio * Time.deltaTime);
+                    break;
+            }
+        }
+
         switch (state)
         {
 
             case State.normal:
                 Rote(terminus, currentYAngle);
-                transform.position = terminus.transform.position;
+                //transform.position = terminus.transform.position;
                 transform.rotation = terminus.transform.rotation;
                 break;
             case State.Lerp:
                 Rote(terminus, currentYAngle);
                 transform.rotation = terminus.transform.rotation;
-                transform.position = Vector3.Lerp(transform.position, terminus.transform.position, ratio*Time.deltaTime);
+                //transform.position = Vector3.Lerp(transform.position, terminus.transform.position, ratio);
                 break;
             case State.LerpMove:
-                transform.position = Vector3.Lerp(transform.position, terminus.transform.position, ratio * Time.deltaTime);
-                Rote(gameObject,currentYAngle);
+                //transform.position = Vector3.Lerp(transform.position, terminus.transform.position, ratio);
+                Rote(gameObject, currentYAngle);
                 Rote(terminus, currentYAngle);
                 break;
         }
+
         //カメラの位置が決定してからプレイヤーの向きを決めることで、カクつきをなくす。
         target.transform.rotation = Quaternion.Euler(0, transform.localEulerAngles.y, 0);
         targetPos = target.position;
@@ -90,6 +133,19 @@ public class CameraFollow : MonoBehaviour
             obj.transform.RotateAround(target.position, obj.transform.right, _playerData.GetRoteAxis().y * Time.deltaTime * roteYSpeed);
         }
 
+    }
+    protected bool WallHitCheck()
+    {
+        if (Physics.Raycast(targetPos, terminus.transform.position - targetPos, out hit, Vector3.Distance(targetPos, terminus.transform.position), wallLayer, QueryTriggerInteraction.Ignore))
+        {
+            wallHitPos = hit.point * 0.95f;//当たった場所に飛ばすとカメラが壁の中に埋まるので調整。
+            Logger.Log(hit.point);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
 
