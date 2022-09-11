93e60cd04a626344d367e3e7f289fe20f9f21537
GTM106 <raito125dragon7@gmail.com>
2022-09-11 14:48:11 +0900

    カーソル操作処理を実装しました

--- Assets/Scripts/UI/Option/SoundSettingManager.cs
+++ Assets/Scripts/UI/Option/SoundSettingManager.cs
@@ -1,11 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundSettingManager : MonoBehaviour
 {
+    enum SoundChoices
+    {
+        None,
+
+        Master,
+        SE,
+        Voice,
+        BGM,
+
+        Max,
+    }
+
     //表示・非表示切り替え用に管理するもの
     [SerializeField] Canvas _canvas = default!;
+    [SerializeField] Image[] _images;
+
+    SoundChoices _choice = SoundChoices.None + 1;
+
+    private void Start()
+    {
+        InputSystemManager.Instance.onNavigatePerformed += OnNavigate;
+    }
+
+    public void OnChangeScene()
+    {
+        InputSystemManager.Instance.onNavigatePerformed -= OnNavigate;
+    }
 
     /// <summary>
     /// Canvasの表示・非表示を設定する関数
@@ -13,5 +39,44 @@ public class SoundSettingManager : MonoBehaviour
     public void SetCanvasEnable(bool enable)
     {
         _canvas.enabled = enable;
+
+        if (enable)
+        {
+            _images[(int)_choice - 1].color = new Color32(176, 176, 176, 255);
+            _choice = SoundChoices.None + 1;
+            _images[(int)_choice - 1].color = Color.red;
+        }
     }
+
+    private void OnNavigate()
+    {
+        if (!_canvas.enabled) return;
+
+        Vector2 axis = InputSystemManager.Instance.NavigateAxis;
+        if (axis != Vector2.up && axis != Vector2.down) return;
+
+        if (axis == Vector2.up)
+        {
+            _choice--;
+            if (_choice <= SoundChoices.None)
+            {
+                _choice = SoundChoices.None + 1;
+                return;
+            }
+        }
+        else if (axis == Vector2.down)
+        {
+            _choice++;
+            if (_choice >= SoundChoices.Max)
+            {
+                _choice = SoundChoices.Max - 1;
+                return;
+            }
+        }
+
+        _images[(int)_choice - 1 + (int)axis.y].color = new Color32(176, 176, 176, 255);
+        _images[(int)_choice - 1].color = Color.red;
+        SoundManager.Instance.PlaySE(SoundSource.SE16_UI_SELECTION);
+    }
+
 }
