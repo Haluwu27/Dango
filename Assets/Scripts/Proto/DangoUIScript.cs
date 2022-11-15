ce87aaeb927d96570058634c56c9eb0e7bd6b43f
Ryouta32 <90999621+Ryouta32@users.noreply.github.com>
2022-11-15 16:00:11 +0900

    団子UIの修正

--- Assets/Scripts/Proto/DangoUIScript.cs
+++ Assets/Scripts/Proto/DangoUIScript.cs
@@ -32,19 +32,20 @@ public class DangoUIScript : MonoBehaviour
             //団子の種類をみてマテリアルに色を付ける、画像が出来たらimagを切り替える。
             //団子が刺さっていないものがあれば非アクティブに
             Objs[i].SetActive(true);
-            DangoImagObjs[i].sprite = DangoImags[(int)dangos[i] - 1];
-            //DangoImagObjs[i].sprite = dangos[i] switch
-            //{
-            //    DangoColor.Red => DangoImags[(int)DangoColor.Red],
-            //    DangoColor.Orange => DangoImags[(int)DangoColor.Orange],
-            //    DangoColor.Yellow => DangoImags[(int)DangoColor.Yellow],
-            //    DangoColor.Green => DangoImags[(int)DangoColor.Green],
-            //    DangoColor.Cyan => DangoImags[(int)DangoColor.Cyan],
-            //    DangoColor.Blue => DangoImags[(int)DangoColor.Blue],
-            //    DangoColor.Purple => DangoImags[(int)DangoColor.Purple],
-            //    DangoColor.Other => DangoImags[(int)DangoColor.Other],
-            //    _ => DangoImags[(int)DangoColor.Other],
-            //};
+            //DangoImagObjs[i].sprite = DangoImags[(int)dangos[i] - 1];
+            DangoImagObjs[i].sprite = dangos[i] switch
+            {
+                DangoColor.Red => DangoImags[(int)DangoColor.Red],
+                DangoColor.Orange => DangoImags[(int)DangoColor.Orange],
+                DangoColor.Yellow => DangoImags[(int)DangoColor.Yellow],
+                DangoColor.Green => DangoImags[(int)DangoColor.Green],
+                DangoColor.Cyan => DangoImags[(int)DangoColor.Cyan],
+                DangoColor.Blue => DangoImags[(int)DangoColor.Blue],
+                DangoColor.Purple => DangoImags[(int)DangoColor.Purple],
+                DangoColor.Other => DangoImags[(int)DangoColor.Other],
+                _ => DangoImags[(int)DangoColor.Other],
+            };
+            Logger.Log((int)dangos[i]);
         }
         for (int i = dangos.Count; i < Objs.Length; i++)
         {
