d8068adb7b31c19981bef2aeb9c1bcec6de2bf5c
2127002 <104291548+2127002@users.noreply.github.com>
2022-12-03 21:34:17 +0900

    Update StageTutorialData.cs

--- Assets/Scripts/GameProgress/StageTutorialData.cs
+++ Assets/Scripts/GameProgress/StageTutorialData.cs
@@ -21,17 +21,39 @@ public class StageTutorialData : StageData.IAddQuest, StageData.IPortrait
 
         List<QuestData> quest = new()
         {
-            questManager.Creater.CreateQuestPlayAction(0,QuestPlayAction.PlayerAction.Stab,1,"団子をさす",0,false,false,new(new PortraitTextData.PTextData(0,"お見事。刺した団子は画面右に出てるからな",5f,PortraitTextData.FacePatturn.Normal),new(1,"次に集めた団子を食べてみろ。団子を集めて『食べる』ボタンだ",10f,PortraitTextData.FacePatturn.Normal)),1),
+            questManager.Creater.CreateQuestPlayAction(0,QuestPlayAction.PlayerAction.Stab,1,"団子を刺す",0,false,false,new(
+                new PortraitTextData.PTextData(0,"お見事。刺した団子は画面右に出てるからな",5f,PortraitTextData.FacePatturn.Normal),
+                new(1,"次に集めた団子を食べてみろ。団子を集めて『食べる』ボタンだ",10f,PortraitTextData.FacePatturn.Normal)),
+                1),
 
-            questManager.Creater.CreateQuestEatDango(1, 3, 0, true, true, "団子を3つ食べる", 0f, true, false,new(new PortraitTextData.PTextData(0,"くぅー美味い！良い調子だ",5f,PortraitTextData.FacePatturn.Normal)), 2,3),
+            questManager.Creater.CreateQuestEatDango(1, 3, 0, true, true, "団子を3つ食べる", 0f, true, false,new(
+                new PortraitTextData.PTextData(0,"くぅー美味い！良い調子だ",5f,PortraitTextData.FacePatturn.Normal),
+                new(1,"流儀を以て、団子を食す、これが『団道』だ",5f,PortraitTextData.FacePatturn.Normal),
+                new(2, "『団道』を達成すると、腹が膨れるだけじゃなく、串が伸びたり、色々と恩恵がある", 10f, PortraitTextData.FacePatturn.Normal),
+                new(3, "さて、ただ食べるだけってのは味気ねぇ、次は『団結』を作ってみろ", 10f, PortraitTextData.FacePatturn.Normal),
+                new(4, "『団結』には色々と種類があるが、基本的には規則正しく並べてやればいい", 10f, PortraitTextData.FacePatturn.Normal)),
+                2,3),
 
-            questManager.Creater.CreateQuestCreateRole(2, new QuestCreateRole.EstablishRole(true, false), 1, 0, "規則正しく並べて『団結』を作れ", 0, false, false,new(new PortraitTextData.PTextData(0,"見事！団子は基本的に『団結』を作って食べた方が腹持ちがいい",10f,PortraitTextData.FacePatturn.Normal),new(0,"次は『高跳び』ボタンで高いところに行ってみるぞ",10f,PortraitTextData.FacePatturn.Normal)), 4),
-            questManager.Creater.CreateQuestCreateRole(3, new QuestCreateRole.EstablishRole(false, false), 1, 0, "ここにヒントを出してみるといいかもな！", 0, false, false,new(new PortraitTextData.PTextData(0,"惜しいな、もうちょっと規則正しく並べてみろ！",8f,PortraitTextData.FacePatturn.Normal)), 2, 3),
+            questManager.Creater.CreateQuestCreateRole(2, new QuestCreateRole.EstablishRole(true, false), 1, 0, "規則正しく並べて『団結』を作れ", 0, false, false,new(
+                new PortraitTextData.PTextData(0,"見事！団子は基本的に『団結』を作って食べた方が腹持ちがいい",10f,PortraitTextData.FacePatturn.Normal),
+                new(1,"次は『高跳び』ボタンで高いところに行ってみるぞ",10f,PortraitTextData.FacePatturn.Normal)),
+                4),
+            
+            questManager.Creater.CreateQuestCreateRole(3, new QuestCreateRole.EstablishRole(false, false), 1, 0, "※同じものだけを集めたり、線対称を作ってみよう", 0, false, false,new(
+                new PortraitTextData.PTextData(0,"惜しいな、もうちょっと規則正しく並べてみろ！",8f,PortraitTextData.FacePatturn.Normal)),
+                2, 3),
 
-            questManager.Creater.CreateQuestDestination(4, FloorManager.Floor.floor2, false, "高台に向かえ", 30f, true, false,new(new PortraitTextData.PTextData()), 5),
+            questManager.Creater.CreateQuestDestination(4, FloorManager.Floor.floor2, false, "高台に向かえ", 30f, true, false,new(
+                new PortraitTextData.PTextData()),
+                5),
 
-            questManager.Creater.CreateQuestPlayAction(5, QuestPlayAction.PlayerAction.FallAttack, 1, "急降下で団子を刺す", 0f, true, true,new(new PortraitTextData.PTextData()), 6),
-            questManager.Creater.CreateQuestDestination(6, FloorManager.Floor.Max, false, "クリア！", 0f, false, true,new(new PortraitTextData.PTextData()), 0),
+            questManager.Creater.CreateQuestPlayAction(5, QuestPlayAction.PlayerAction.FallAttack, 1, "急降下で団子を刺す", 0f, true, true,new(
+                new PortraitTextData.PTextData()),
+                6),
+
+            questManager.Creater.CreateQuestDestination(6, FloorManager.Floor.Max, false, "初心者指南完了！", 0f, false, true,new(
+                new PortraitTextData.PTextData()),
+                0),
         };
 
         QuestData.Clear();
